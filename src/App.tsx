35cabbb3505217222b6a3c4a79a1d4b6a9d81fc7
romansen47 <romansen47@gmail.com>
2026-08-21 21:11:49 +0200

    Render analysis evaluation output

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import AnalysisEvaluationOutputPortal from "./AnalysisEvaluationOutputPortal";
 import { ChessBoard } from "./ChessBoard";
 
 function App() {
   return (
     <div className="app">
       <ChessBoard />
+      <AnalysisEvaluationOutputPortal />
     </div>
   );
 }
