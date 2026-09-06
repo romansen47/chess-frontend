d4a1671e947f02713367202b8cfc1079ea9ffafc
romansen47 <romansen47@gmail.com>
2026-09-06 22:36:05 +0200

    Enable asynchronous board move animation timing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./boardMoveAnimation.css";
+import { installBoardMoveAnimationTiming } from "./boardMoveAnimation";
+
+installBoardMoveAnimationTiming();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
