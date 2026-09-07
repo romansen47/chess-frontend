4d5368c86c7a94c27614a74fc7f19a4c7e44a6f4
romansen47 <romansen47@gmail.com>
2026-09-07 16:14:55 +0200

    Apply selected locale to browser dialogs and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import AnalysisEvaluationLifecycleGuard from "./AnalysisEvaluationLifecycleGuard";
 import AnalysisEvaluationOutputPortal from "./AnalysisEvaluationOutputPortal";
 import { ChessBoard } from "./ChessBoard";
+import BrowserLocaleBridge from "./i18n/BrowserLocaleBridge";
 import { I18nProvider } from "./i18n/I18nProvider";
 import LanguageSelector from "./i18n/LanguageSelector";
 
 function App() {
   return (
     <I18nProvider>
+      <BrowserLocaleBridge />
       <div className="app">
         <LanguageSelector />
         <ChessBoard />
