6e60c4905cafa04f1652582923cf3be20a420bd6
romansen47 <romansen47@gmail.com>
2026-09-07 16:13:28 +0200

    Enable browser localization and language selector

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import "./App.css";
 import AnalysisEvaluationLifecycleGuard from "./AnalysisEvaluationLifecycleGuard";
 import AnalysisEvaluationOutputPortal from "./AnalysisEvaluationOutputPortal";
 import { ChessBoard } from "./ChessBoard";
+import { I18nProvider } from "./i18n/I18nProvider";
+import LanguageSelector from "./i18n/LanguageSelector";
 
 function App() {
   return (
-    <div className="app">
-      <ChessBoard />
-      <AnalysisEvaluationLifecycleGuard />
-      <AnalysisEvaluationOutputPortal />
-    </div>
+    <I18nProvider>
+      <div className="app">
+        <LanguageSelector />
+        <ChessBoard />
+        <AnalysisEvaluationLifecycleGuard />
+        <AnalysisEvaluationOutputPortal />
+      </div>
+    </I18nProvider>
   );
 }
 
