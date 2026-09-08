f290bd70eb85ce4609faca6fddbf74f0ff0f56c7
romansen47 <romansen47@gmail.com>
2026-09-08 22:33:19 +0200

    Enable SSE live evaluation bridge

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AnalysisEvaluationLifecycleGuard from "./AnalysisEvaluationLifecycleGuard
 import AnalysisEvaluationOutputPortal from "./AnalysisEvaluationOutputPortal";
 import { ChessBoard } from "./ChessBoard";
 import EngineProfileContextMenu from "./EngineProfileContextMenu";
+import LiveEvaluationSseBridge from "./LiveEvaluationSseBridge";
 import BrowserLocaleBridge from "./i18n/BrowserLocaleBridge";
 import { I18nProvider } from "./i18n/I18nProvider";
 
@@ -15,6 +16,7 @@ function App() {
         <AnalysisEvaluationLifecycleGuard />
         <AnalysisEvaluationOutputPortal />
         <EngineProfileContextMenu />
+        <LiveEvaluationSseBridge />
       </div>
     </I18nProvider>
   );
